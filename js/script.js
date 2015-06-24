5a994a9d01b5e1e451416d27bd36a7b3daaf8522
Eric <eprewitt@prokarma.com>
2015-06-24 12:04:26 -0600

    adding extra number and combo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var entry = [],
-	right = ["3", "2", "1"],
+	right = ["6", "3", "7", "8"],
     combo = [],
     notes = [],
     winnerText = $(".notify");
@@ -7,16 +7,17 @@ var entry = [],
 winnerText.hide();
 
 $("a").click(function(e){
-	var chk = $(this).hasClass("pressed"),
-	    ths = $(this),
-	    num = ths.text();
-	if ( !chk ) {
-		ths.addClass("pressed");
-	}
+	// var chk = $(this).hasClass("pressed"),
+	   var ths = $(this)
+	   var num = ths.text();
+	// if ( !chk ) {
+	// 	ths.addClass("pressed");
+	// }
 	combo.plus(num);
 	$(".num1").text(entry[0]);
 	$(".num2").text(entry[1]);
 	$(".num3").text(entry[2]);
+	$(".num4").text(entry[3]);
 	console.log(entry[0]);
 	console.log(entry);
 });
@@ -24,7 +25,7 @@ $("a").click(function(e){
 combo.plus = function(t) {
 	winner = false;
 	entry.push(t);
-	if (entry.length > 3) {
+	if (entry.length > 4) {
 		combo.check();
 		//update notepad
 		$(".notepad").append("<p>" + entry + "</p>");
